82b1df57283ee30018f4e419ba92aef244a3ee95
My Name <my@email.com>
2024-05-12 00:03:47 +0400

    task1 is done

--- HW/hw_section_3/hw_3_4_adapters_and_views/task1.cpp
+++ HW/hw_section_3/hw_3_4_adapters_and_views/task1.cpp
@@ -10,3 +10,38 @@
 
 Напечатайте YES, если скобочная последовательность является правильной, и NO в противном случае.
 */
+#include <iostream>
+#include <string>
+#include <stack>
+
+bool CorrectSequence(const std::string& bracketSequnce) {
+    std::stack<char> openingBrackets;
+
+    for (char bracket : bracketSequnce) {
+        if (bracket == '[' || bracket == '{' || bracket == '(') {
+            openingBrackets.push(bracket);
+        } else {
+            if (openingBrackets.empty()) {
+                return false;
+            }
+            char top = openingBrackets.top();
+            if ((top == '[' && bracket == ']') || (top == '{' && bracket == '}') || (top == '(' && bracket == ')')) {
+                openingBrackets.pop();
+            } else {
+                return false;
+            }
+        }
+    }
+
+    return openingBrackets.empty();
+}
+
+int main() {
+    std::string bracketSequence;
+    std::getline(std::cin, bracketSequence);
+    if (CorrectSequence(bracketSequence)) {
+        std::cout << "YES\n";
+    } else {
+        std::cout << "NO\n";
+    }
+}
