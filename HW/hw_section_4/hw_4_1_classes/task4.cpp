30f6f152349f9cbdaddf8a269fbf0b7edb886643
My Name <my@email.com>
2024-07-12 23:13:47 +0400

    task4 is being done

--- HW/hw_section_4/hw_4_1_classes/task4.cpp
+++ HW/hw_section_4/hw_4_1_classes/task4.cpp
@@ -36,3 +36,23 @@ public:
 
 Можно считать, что вектор, передаваемый на вход этих функций, всегда непустой.
 */
+#include <map>
+#include <string>
+#include <vector>
+
+struct Node {
+    std::map<std::string, Node> children;
+};
+
+class Tree {
+private:
+    Node root;
+
+public:
+    bool Has(const std::vector<std::string>& node) const;
+    void Insert(const std::vector<std::string>& node);
+    void Delete(const std::vector<std::string>& node);
+};
+
+// Ваш код будет вставлен сюда
+// #include "your_code"
