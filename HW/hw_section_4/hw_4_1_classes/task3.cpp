8c76c51ccffa5814abacd77424b9f3315898e8c5
My Name <my@email.com>
2024-07-12 22:24:17 +0400

    task3 is being done

--- HW/hw_section_4/hw_4_1_classes/task3.cpp
+++ HW/hw_section_4/hw_4_1_classes/task3.cpp
@@ -11,3 +11,48 @@
 этой дроби (знаменатель должен быть положительным). 
 Переопределите также операторы +=, -=, *= и /=. Не забудьте определить операторы == и !=.
 */
+#include <numeric>
+#include <iostream>
+
+class Rational {
+private:
+    int numerator{0};
+    int denominator{1};
+
+    void Reduce() {
+        int divisor = std::gcd(numerator, denominator);
+        numerator /= divisor;
+        denominator /= divisor;
+        if (denominator < 0) {
+            numerator *= -1;
+            denominator *= -1;
+        }
+    }
+
+public:
+    Rational(int n, int d) {
+        numerator = n;
+        denominator = d;
+        Reduce();
+    }
+    Rational() = default;
+
+    int GetNumerator() {
+        return numerator;
+    }
+
+    int GetDenominator() {
+        return denominator;
+    }
+};
+
+int main() {
+    // int a = 5;
+    // int b = -10;
+    // int res = std::gcd(a, b);
+    // int r = b / res;
+    // std::cout << r;
+    Rational r(6, 9);
+    std::cout << r.GetNumerator() << "/" << r.GetDenominator();
+
+}
