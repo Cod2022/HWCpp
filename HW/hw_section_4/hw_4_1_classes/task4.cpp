fa98be74d69ce76221a808e6a286a8d4f617a413
My Name <my@email.com>
2024-07-15 22:00:02 +0400

    done

--- HW/hw_section_4/hw_4_1_classes/task4.cpp
+++ HW/hw_section_4/hw_4_1_classes/task4.cpp
@@ -49,40 +49,48 @@ private:
     Node root;
 
 public:
-    bool Has(const std::vector<std::string>& node) const {
-        const Node* temp = &root;
-        for (const auto& elem : node) {
-            if(!temp->children.contains(elem)) {
-                return false;
-            }
-            temp = &temp->children.at(elem);
-        }
-        return true;
-    };
-
-    void Insert(const std::vector<std::string>& node) {
-        Node* temp = &root;
-        for (const auto& elem : node) {
-            if(!temp->children.contains(elem)) {
-                temp->children[elem];
-            }
-            temp = &temp->children.at(elem);
-        }
-
-    };
-
-    void Tree::Delete(const std::vector<std::string>& node) {
-        Node* temp = &root;
-        for (size_t i = 0; i < node.size(); ++i) {
-            const auto& elem = node[i];
-            if (!temp->children.contains(elem)) {
-                return;
-                }
-            if (i + 1 == node.size()) {
-                temp->children.erase(elem);
-            } else {
-                temp = &temp->children.at(elem);
-                }
-            }
-    }
+    bool Has(const std::vector<std::string>& node) const;
+    void Insert(const std::vector<std::string>& node);
+    void Delete(const std::vector<std::string>& node); 
 };
+
+bool Tree::Has(const std::vector<std::string>& node) const {
+    const Node* temp = &root;
+    for (const auto& elem : node) {
+        if(!temp->children.contains(elem)) {
+            return false;
+        }
+        temp = &temp->children.at(elem);
+    }
+    return true;
+}
+
+void Tree::Insert(const std::vector<std::string>& node) {
+    Node* temp = &root;
+    for (const auto& elem : node) {
+        if(!temp->children.contains(elem)) {
+            temp->children[elem];
+        }
+        temp = &temp->children.at(elem);
+    }
+
+}
+
+void Tree::Delete(const std::vector<std::string>& node) {
+    Node* temp = &root;
+    for (size_t i = 0; i < node.size(); ++i) {
+        const auto& elem = node[i];
+        if (!temp->children.contains(elem)) {
+            return;
+            }
+        if (i + 1 == node.size()) {
+            temp->children.erase(elem);
+        } else {
+            temp = &temp->children.at(elem);
+            }
+        }
+    }
+
+    int main() {
+
+    }
