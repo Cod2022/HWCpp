cc9b98b9c3c8de60f6554ab02f1817431e5260d4
My Name <my@email.com>
2024-08-09 01:40:12 +0400

    task4 is being done

--- HW/hw_section_4/hw_4_2_classes_templates/task4.cpp
+++ HW/hw_section_4/hw_4_2_classes_templates/task4.cpp
@@ -45,3 +45,37 @@ public:
     void PushBack(const T& elem);
 };
 */
+#include <vector>
+
+template <typename T>
+class Deque {
+private:
+    std::vector<T> head, tail;
+
+public:
+    bool Empty() const;
+
+    size_t Size() const;
+
+    void Clear();
+
+    const T& operator [] (size_t i) const;
+
+    T& operator [] (size_t i);
+
+    const T& At(size_t i) const;  // throws std::out_of_range on incorrect index
+
+    T& At(size_t i);  // throws std::out_of_range on incorrect index
+
+    const T& Front() const;
+
+    T& Front();
+
+    const T& Back() const;
+
+    T& Back();
+
+    void PushFront(const T& elem);
+
+    void PushBack(const T& elem);
+};
