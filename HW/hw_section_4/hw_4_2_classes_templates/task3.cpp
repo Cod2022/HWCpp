7c8f2fdb683edbb5ba445364f8c8a2083fea5288
My Name <my@email.com>
2024-08-09 01:18:51 +0400

    task3 is done

--- HW/hw_section_4/hw_4_2_classes_templates/task3.cpp
+++ HW/hw_section_4/hw_4_2_classes_templates/task3.cpp
@@ -69,3 +69,42 @@ int main() {
     res = kv.Find("hello", nullptr);  // должно вернуться true
 }
 */
+#include <unordered_map>
+#include <algorithm>
+#include <iostream>
+
+template <typename Key, typename Value>
+class KeyValueStorage {
+private:
+      std::unordered_map<Key, Value> data;
+
+public:
+    void Insert(const Key& key, const Value& value) {
+        data[key] = value;
+    }
+
+    void Remove(const Key& key) {
+        data.erase(key);
+    }
+
+    bool Find(const Key& key, Value* value = nullptr) const;
+};
+
+template <typename Key, typename Value>
+bool KeyValueStorage<Key, Value>::Find(const Key& key, Value* value) const {
+    auto it = data.find(key);
+    if (value != nullptr && it != data.end()) {
+        *value = it->second;
+    }
+    return it != data.end();
+}
+
+int main() {
+    KeyValueStorage<std::string, int> kv;
+    kv.Insert("hello", 42);
+    kv.Insert("bye", -13);
+    int value = 123;
+    auto res = kv.Find("wrong", &value);  // должно вернуться false, а value не должен меняться
+    res = kv.Find("bye", &value);  // должно вернуться true, в value должно быть -13
+    res = kv.Find("hello", nullptr);  // должно вернуться true
+}
