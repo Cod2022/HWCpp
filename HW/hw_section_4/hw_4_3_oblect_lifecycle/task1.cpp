1b90826c069020f547e528d9f028340e3a426ac5
My Name <my@email.com>
2024-08-17 03:38:34 +0400

    changed task1

--- HW/hw_section_4/hw_4_3_oblect_lifecycle/task1.cpp
+++ HW/hw_section_4/hw_4_3_oblect_lifecycle/task1.cpp
@@ -2,43 +2,42 @@
 Вам дан готовый класс Logger, который в своих конструкторах, 
 операторах присваивания и деструкторе печатает соответствующие сообщения:
 */
-#include <iostream>
+// #include <iostream>
 
-class Logger {
-private:
-    static int counter;
-    const int id;
+// class Logger {
+// private:
+//     static int counter;
+//     const int id;
 
-public:
-    Logger(): id(++counter) {
-        std::cout << "Logger(): " << id << "\n";
-    }
+// public:
+//     Logger(): id(++counter) {
+//         std::cout << "Logger(): " << id << "\n";
+//     }
 
-    Logger(const Logger& other): id(++counter) {
-        std::cout << "Logger(const Logger&): " << id << " " << other.id << "\n";
-    }
+//     Logger(const Logger& other): id(++counter) {
+//         std::cout << "Logger(const Logger&): " << id << " " << other.id << "\n";
+//     }
 
-    Logger(Logger&& other): id(++counter) {
-        std::cout << "Logger(Logger&&): " << id << " " << other.id << "\n";
-    }
+//     Logger(Logger&& other): id(++counter) {
+//         std::cout << "Logger(Logger&&): " << id << " " << other.id << "\n";
+//     }
 
-    Logger& operator = (const Logger& other) {
-        std::cout << "Logger& operator = (const Logger&): " << id << " " << other.id << "\n";
-        return *this;
-    }
+//     Logger& operator = (const Logger& other) {
+//         std::cout << "Logger& operator = (const Logger&): " << id << " " << other.id << "\n";
+//         return *this;
+//     }
 
-    Logger& operator = (Logger&& other) {
-        std::cout << "Logger& operator = (Logger&&): " << id << " " << other.id << "\n";
-        return *this;
-    }
+//     Logger& operator = (Logger&& other) {
+//         std::cout << "Logger& operator = (Logger&&): " << id << " " << other.id << "\n";
+//         return *this;
+//     }
 
-    ~Logger() {
-        std::cout << "~Logger(): " << id << "\n";
-    }
-};
+//     ~Logger() {
+//         std::cout << "~Logger(): " << id << "\n";
+//     }
+// };
 
-int Logger::counter = 0;
-
+// int Logger::counter = 0;
 /*
 Вам требуется написать программу, которая работает с этим классом и выводит следующий текст:
 
@@ -47,6 +46,7 @@ Logger(): 1
 Logger(): 2
 ~Logger(): 2
 */
+#include "logger.h"
 
 int main() {
     Logger();
