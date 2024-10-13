f350659f95af20d4a0526d4c5ddf2a72b2bc7e57
My Name <my@email.com>
2024-10-14 01:54:45 +0400

    task2 is done

--- HW/hw_section_4/hw_4_4_inheritance_and_polymorphism/task2.cpp
+++ HW/hw_section_4/hw_4_4_inheritance_and_polymorphism/task2.cpp
@@ -38,3 +38,67 @@ int main() {
 чтобы приведённый код заработал. Функция Notify 
 в этих классах должна вызывать данные вам функции SendSms или SendEmail.
 */
+#include <iostream>
+#include <string>
+
+void SendSms(const std::string& number, const std::string& message) {
+        std::cout << "Send '" << message << "' to number " << number << std::endl;
+}
+
+void SendEmail(const std::string& email, const std::string& message) {
+    std::cout << "Send '" << message << "' to e-mail "  << email << std::endl;
+}
+
+class NotifierBase {
+public:
+
+    virtual void Notify(const std::string& message) const = 0;
+
+    virtual ~NotifierBase() {
+    }
+
+    
+};
+
+class SmsNotifier: public NotifierBase {
+private:
+
+    std::string sms;
+
+public:
+
+    SmsNotifier(const std::string& n): sms(n) {
+    }
+
+    void Notify(const std::string& message) const override {
+        SendSms(sms, message);
+    }
+};
+
+class EmailNotifier: public NotifierBase {
+private:
+
+    std::string email;
+
+public:
+
+    EmailNotifier(const std::string& e): email(e) {
+    }
+
+    void Notify(const std::string& message) const override {
+        SendEmail(email, message);
+    }
+};
+
+void Notify(const NotifierBase& notifier, const std::string& message) {
+    notifier.Notify(message);
+}
+
+int main() {
+    SmsNotifier sms("+7-495-777-77-77");
+    EmailNotifier email("na-derevnyu@dedushke.ru");
+
+    Notify(sms, "Hello! How are you?");
+    Notify(email, "Let's learn C++!");
+    return 0;
+}
