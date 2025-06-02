fcd558a93298a0af7a4896b071531ecb61eaef2b
My Name <my@email.com>
2025-06-02 21:37:43 +0400

    code corrected, task6 is done

--- HW/hw_section_4/hw_4_6_RAII_idiom_and_smart_pointers/task6.cpp
+++ HW/hw_section_4/hw_4_6_RAII_idiom_and_smart_pointers/task6.cpp
@@ -14,19 +14,21 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <memory>
 
 class Document {
 protected:
     const std::string Content;
 public:
     Document(const std::string& s): Content(s) {}
-    void Save() const {}
+    virtual void Save() const {}
+    virtual ~Document() {}
 };
 
 class PlainTextDocument: public Document {
 public:
     PlainTextDocument(const std::string& s): Document(s) {}
-    virtual void Save() {
+    void Save() const override {
         std::cout << Content << "\n";
     }
 };
@@ -34,30 +36,31 @@ public:
 class HTMLDocument: public Document {
 public:
     HTMLDocument(const std::string& s): Document(s) {}
-    virtual void Save() {
-        std::cout << "<HTML><BODY>" << Content << "</BODY/></HTML>\n";
+    void Save() const override {
+        std::cout << "<HTML><BODY>" << Content << "</BODY></HTML>\n";
     }
 };
 
-using DocumentCollection = std::vector<Document>;
+using DocumentCollection = std::vector<std::unique_ptr<Document>>;
 
 void AddDocument(const std::string& content, const std::string& type,
 DocumentCollection& collection) {
     if (type == "plain") {
-        collection.push_back(PlainTextDocument(content));
+        collection.emplace_back(std::make_unique<PlainTextDocument>(content));
     } else if (type == "html") {
-        collection.push_back(HTMLDocument(content));
+        collection.emplace_back(std::make_unique<HTMLDocument>(content));
     }
 }
 
 void PrintCollection(const DocumentCollection& collection) {
     for (const auto& doc : collection) {
-        doc.Save();
+        doc->Save();
     }
 }
 
 int main() {
     DocumentCollection collection;
     AddDocument("content", "plain", collection);
+    AddDocument("html", "html", collection);
     PrintCollection(collection);
 }
