3b418901c347d16d7dffb5de0b7ba0f5a44240fa
My Name <my@email.com>
2025-06-02 21:10:34 +0400

    added original code to be corrected

--- HW/hw_section_4/hw_4_6_RAII_idiom_and_smart_pointers/task6.cpp
+++ HW/hw_section_4/hw_4_6_RAII_idiom_and_smart_pointers/task6.cpp
@@ -11,3 +11,53 @@
 Второй параметр функции AddDocument может принимать значения "plain" или "html".
 5.Утечек памяти быть не должно.
 */
+#include <iostream>
+#include <string>
+#include <vector>
+
+class Document {
+protected:
+    const std::string Content;
+public:
+    Document(const std::string& s): Content(s) {}
+    void Save() const {}
+};
+
+class PlainTextDocument: public Document {
+public:
+    PlainTextDocument(const std::string& s): Document(s) {}
+    virtual void Save() {
+        std::cout << Content << "\n";
+    }
+};
+
+class HTMLDocument: public Document {
+public:
+    HTMLDocument(const std::string& s): Document(s) {}
+    virtual void Save() {
+        std::cout << "<HTML><BODY>" << Content << "</BODY/></HTML>\n";
+    }
+};
+
+using DocumentCollection = std::vector<Document>;
+
+void AddDocument(const std::string& content, const std::string& type,
+DocumentCollection& collection) {
+    if (type == "plain") {
+        collection.push_back(PlainTextDocument(content));
+    } else if (type == "html") {
+        collection.push_back(HTMLDocument(content));
+    }
+}
+
+void PrintCollection(const DocumentCollection& collection) {
+    for (const auto& doc : collection) {
+        doc.Save();
+    }
+}
+
+int main() {
+    DocumentCollection collection;
+    AddDocument("content", "plain", collection);
+    PrintCollection(collection);
+}
